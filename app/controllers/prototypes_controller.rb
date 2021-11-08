4d64bcc24ec2647a99cce963b76c2e06861281ea
yuka2875 <yuka986532@gmail.com>
2021-11-08 17:10:29 +0900

    詳細ページから削除ボタンをクリックすると投稿が削除できるようにした

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -25,6 +25,29 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params)
+
+    if prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+
+    redirect_to root_path
+    
+  end
+
   private
 
   def prototype_params
