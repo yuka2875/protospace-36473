60bc3023449a3ddd2332b2cec754eb22d6909312
yuka2875 <yuka986532@gmail.com>
2021-11-08 23:31:26 +0900

    コントローラーを作成し、アクションとルーティングを設定した

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :comments, only: [:create]
 
 end
