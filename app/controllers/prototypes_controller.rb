f0373914e6245b32773a92aade9da9e5fb0fc834
yuka2875 <yuka986532@gmail.com>
2021-11-10 11:57:35 +0900

    投稿したコメントが詳細ページで表示されるようにした

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -24,6 +24,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
+    @comments = @prototype.comment.includes(:user)
    
   end
 
