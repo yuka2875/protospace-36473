716095534eac903861e1dfa059f5c138817f0b20
yuka2875 <yuka986532@gmail.com>
2021-11-10 16:34:34 +0900

    ログインしていないユーザーのページ遷移を制限した

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+
   def index
     @prototypes = Prototype.all
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @name = @user.name
+    @prototypes = @user.prototype
 
   end
 
