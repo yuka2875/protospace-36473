447ef528367709c513e2544c1ea5e84f2e0d049b
yuka2875 <yuka986532@gmail.com>
2021-11-18 12:38:17 +0900

    修正3

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new,  :destroy]
+  before_action :authenticate_user!, only: [:new, :destroy]
   before_action :move_to_index, except: [:index, :show]
 
   def index
